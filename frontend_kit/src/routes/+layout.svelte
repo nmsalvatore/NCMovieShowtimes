e3dd6e8d989d2a42e51950b1b149425ef96af2cc
Nick Salvatore <nicholassalvatore@Nicholass-MacBook-Air.local>
2023-10-23 08:15:10 -0700

    Move global css to static file

--- frontend_kit/src/routes/+layout.svelte
+++ frontend_kit/src/routes/+layout.svelte
@@ -4,28 +4,6 @@
 
 
 <Header />
-
 <main>
     <slot />
 </main>
-
-
-<style>
-    :global(*) {
-        padding: 0;
-        margin: 0;
-        box-sizing: border-box;
-    }
-
-    :global(body) {
-        color: #333;
-        font-family: Helvetica, sans-serif;
-        letter-spacing: 0.3px;
-        max-width: 960px;
-        margin: 0 auto;
-    }
-
-    :global(ul) {
-        list-style-type: none;
-    }
-</style>
