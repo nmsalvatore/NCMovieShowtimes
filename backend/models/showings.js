751e5aa3bff8bf06f09cfba7ddb315cc4122d658
Nick Salvatore <nicholassalvatore@Nicholass-MacBook-Air.local>
2023-10-23 08:21:08 -0700

    Add venue name to showings model

--- backend/models/showings.js
+++ backend/models/showings.js
@@ -2,7 +2,7 @@ import pool from '../config/db.js'
 
 export const getShowingsByDate = async date => {
     const result = await pool.query(
-        `SELECT showings.time, showings.date, movies.movie_title, venues.venue_name, showings.url
+        `SELECT showings.time, showings.date, movies.movie_title, venues.venue_name, venues.city, showings.url
          FROM showings
          JOIN movies ON showings.movie_id = movies.movie_id
          JOIN venues ON showings.venue_id = venues.venue_id
