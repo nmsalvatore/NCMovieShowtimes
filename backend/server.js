5289022602e1af48333c838cde6c3fb41ff08d19
Nick Salvatore <nicholassalvatore@Nicholass-MacBook-Air.local>
2023-11-20 18:08:34 -0800

    Fixed spacing in windowMs equation

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ const corsOptions = {
 }
 
 const limiter = rateLimit({
-    windowMs: 15 *60 * 1000,
+    windowMs: 15 * 60 * 1000,
     limit: 100,
     standardHeaders: 'draft-7',
     legacyHeaders: false
