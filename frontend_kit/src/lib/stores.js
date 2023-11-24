c821997a25c6fa061273b91c2e69ca1bfbc36552
Nick Salvatore <nicholassalvatore@Nicholass-MacBook-Air.local>
2023-11-24 13:17:11 -0800

    Added mobile styling to nav buttons

--- frontend_kit/src/lib/stores.js
+++ frontend_kit/src/lib/stores.js
@@ -2,3 +2,18 @@ import { writable } from 'svelte/store'
 
 export const activeRouteID = writable(null)
 export const activeDate = writable(null)
+export const windowWidth = createWindowWidthStore()
+
+function createWindowWidthStore() {
+    const { subscribe, set } = writable(0)
+
+    if (typeof window !== 'undefined') {
+        set(window.innerWidth)
+
+        window.addEventListener('resize', () => {
+            set(window.innerWidth)
+        })
+    }
+
+    return { subscribe }
+}
