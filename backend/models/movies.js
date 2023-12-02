21a0387339c0aa58219f5fbfb76e36fe6d6c25d7
Nick Salvatore <nicholassalvatore@Nicholass-MacBook-Air.local>
2023-12-02 10:15:45 -0800

    Add runtime and rating to movies model

--- backend/models/movies.js
+++ backend/models/movies.js
@@ -2,7 +2,7 @@ import pool from '../config/db.js'
 
 export const getAll = async () => {
     const result = await pool.query(
-        `SELECT movies.movie_title, venues.venue_name, showings.date
+        `SELECT movies.movie_title, movies.rating, movies.runtime, venues.venue_name, showings.date
         FROM showings
         JOIN movies ON showings.movie_id = movies.movie_id
         JOIN venues ON showings.venue_id = venues.venue_id`
