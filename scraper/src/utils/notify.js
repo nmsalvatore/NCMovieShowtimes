0c37ed61132318b51fff70e2bb8627b740e6f8b3
Nick Salvatore <nicholassalvatore@Nicholass-MacBook-Air.local>
2023-12-12 15:50:17 -0800

    Improved error handling in notify.js
    - Replaced console.error with throw error in sendMail call
    - Replaced console log with logger statement

--- scraper/src/utils/notify.js
+++ scraper/src/utils/notify.js
@@ -1,32 +1,34 @@
 import nodemailer from 'nodemailer'
+import logger from '../utils/logger.js'
 import 'dotenv/config'
 
+function sendEmail(subject, body) {
+    return new Promise((resolve, reject) => {
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.mailgun.org',
+            port: 587,
+            secure: false,
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASSWORD
+            }
+        });
 
-async function sendEmail(subject, body) {
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.mailgun.org',
-        port: 587,
-        secure: false,
-        auth: {
-            user: process.env.SMTP_USER,
-            pass: process.env.SMTP_PASSWORD
-        }
-    })
-      
-    const mailOptions = {
-        from: process.env.VERIFIED_SENDER,
-        to: process.env.VERIFIED_RECIPIENT,
-        subject: subject,
-        html: body
-    }
-      
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.error(error)
-        }
-        
-        console.log('Email sent: ' + info.response)
-    }) 
+        const mailOptions = {
+            from: process.env.VERIFIED_SENDER,
+            to: process.env.VERIFIED_RECIPIENT,
+            subject: subject,
+            html: body
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                throw error
+            }
+            
+            logger.info('Email sent: ' + info.response)
+        }) 
+    });
 }
 
 export const notify = { sendEmail }
