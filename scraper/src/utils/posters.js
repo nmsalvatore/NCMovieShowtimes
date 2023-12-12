735a2abe6e827566be7ea98d9e6d92608438c3e4
Nick Salvatore <nicholassalvatore@Nicholass-MacBook-Air.local>
2023-12-12 15:59:58 -0800

    Removed comments from posters.js

--- scraper/src/utils/posters.js
+++ scraper/src/utils/posters.js
@@ -3,22 +3,17 @@ import { createWriteStream, existsSync } from 'fs';
 import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-// Function to download a movie poster
 export async function downloadMoviePoster(url, filename) {
-    // Generate the file path
     const __dirname = dirname(fileURLToPath(import.meta.url));
     const filepath = join(__dirname, '..', '..', '..', 'posters', filename);
 
-    // Check if the file already exists
     if (existsSync(filepath)) {
         return;
     }
 
-    // Create a write stream
     const writer = createWriteStream(filepath);
 
     try {
-        // Attempt to download the image
         const response = await axios({
             url,
             method: 'GET',
