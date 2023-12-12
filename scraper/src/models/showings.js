cde8e0ab4eb1446f397d593d832384a442ff970e
Nick Salvatore <nicholassalvatore@Nicholass-MacBook-Air.local>
2023-12-12 14:30:40 -0800

    Enhance error handling and logging in showings.js
    - Add try-catch blocks to getShowingID()
    - Add error re-throw to getShowingID()

--- scraper/src/models/showings.js
+++ scraper/src/models/showings.js
@@ -1,5 +1,5 @@
 import * as db from '../config/db.js'
-
+import logger from '../utils/logger.js'
 
 export async function addShowing(movie_id, venue_id, date, time, url) {
     try {
@@ -7,20 +7,25 @@ export async function addShowing(movie_id, venue_id, date, time, url) {
             'INSERT INTO showings (movie_id, venue_id, date, time, url) VALUES ($1, $2, $3, $4, $5)',
             [movie_id, venue_id, date, time, url]
         )
-    } catch (err) {
-        console.error(`Error inserting showing: ${err}`)
+    } catch (error) {
+        logger.error(`Error inserting showing:`, error)
+        throw error
     }
 }
 
-
 export async function getShowingID(movie_id, venue_id, date, time, url) {
-    const result = await db.client.query(
-        'SELECT showing_id FROM showings WHERE movie_id = $1 AND venue_id = $2 AND date = $3 AND time = $4 AND url = $5', 
-        [movie_id, venue_id, date, time, url]
-    )
-
-    if (result.rowCount > 0) {
-        const showingID = result.rows[0].showing_id
-        return showingID
+    try {
+        const result = await db.client.query(
+            'SELECT showing_id FROM showings WHERE movie_id = $1 AND venue_id = $2 AND date = $3 AND time = $4 AND url = $5', 
+            [movie_id, venue_id, date, time, url]
+        )
+    
+        if (result.rowCount > 0) {
+            const showingID = result.rows[0].showing_id
+            return showingID
+        }
+    } catch (error) {
+        logger.error(`Error retrieving showing_id:`, error)
+        throw error
     }
 }
