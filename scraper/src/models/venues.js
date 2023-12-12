fb16f49a31d8f1596175ca3a8a6cf2b79706310c
Nick Salvatore <nicholassalvatore@Nicholass-MacBook-Air.local>
2023-12-12 14:37:03 -0800

    Enhance error handling and logging in venues.js
    - Add try-catch blocks to getVenueID
    - Replace console.error with logger.error
    - Add error re-throw in getVenueID

--- scraper/src/models/venues.js
+++ scraper/src/models/venues.js
@@ -1,25 +1,27 @@
 import * as db from '../config/db.js'
 
-
 export async function addVenue(name, address) {
     try {
         const result = await db.client.query(
             'INSERT INTO venues (venue_name, address) VALUES ($1, $2) ON CONFLICT (venue_name) DO NOTHING RETURNING venue_id',
-            [name, address]
-        )
-
+            [name, address])
         const venueID = result.rows[0].venue_id
         return venueID
-    } catch (err) {
-        console.error(`Error inserting venue: ${err}`)
+    } catch (error) {
+        logger.error(`Error inserting venue "${name}" into database:`,  error)
+        throw error
     }
 }
 
-
 export async function getVenueID(name) {
-    const result = await db.client.query('SELECT venue_id FROM venues WHERE venue_name = $1', [name])
-    if (result.rowCount > 0) {
-        const venueID = result.rows[0].venue_id
-        return venueID
+    try {
+        const result = await db.client.query('SELECT venue_id FROM venues WHERE venue_name = $1', [name])
+        if (result.rowCount > 0) {
+            const venueID = result.rows[0].venue_id
+            return venueID
+        }
+    } catch (error) {
+        logger.error(`Error retrieving venue_id for venue "${name}:`, error)
+        throw error
     }
 }
