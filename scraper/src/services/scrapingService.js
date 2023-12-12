4ba4e112c0e4fc60fa559cd4489eb4abce2db89f
Nick Salvatore <nicholassalvatore@Nicholass-MacBook-Air.local>
2023-12-12 06:58:11 -0800

    Add try/catch to getAllShowings()

--- scraper/src/services/scrapingService.js
+++ scraper/src/services/scrapingService.js
@@ -2,6 +2,7 @@ import { onyx } from '../scrapers/onyx.js'
 import { sierra } from '../scrapers/sierra.js'
 import { mystic } from '../scrapers/mystic.js'
 import startDatabaseUpdateService from './databaseService.js'
+import logger from '../utils/logger.js'
 
 export default async function startScrapingService() {
     const showings = await getAllShowings()
@@ -9,17 +10,19 @@ export default async function startScrapingService() {
 }
 
 async function getAllShowings() {
-    // Get all showings data
-    const onyxShowings = await onyx.getShowings()
-    const sierraShowings = await sierra.getShowings()
-    const mysticShowings = await mystic.getShowings()
-
-    // Concatenate showings data in a single array
-    const showings = [].concat(
-        onyxShowings,
-        sierraShowings,
-        mysticShowings
-    )
-
-    return showings
+    try {
+        const onyxShowings = await onyx.getShowings()
+        const sierraShowings = await sierra.getShowings()
+        const mysticShowings = await mystic.getShowings()
+    
+        const showings = [].concat(
+            onyxShowings,
+            sierraShowings,
+            mysticShowings
+        )
+    
+        return showings
+    } catch (error) {
+        logger.error(error)
+    }
 }
