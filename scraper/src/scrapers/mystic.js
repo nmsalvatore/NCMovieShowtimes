2a075bb79f98634cb1cbb837fa9136c8e674386c
Nick Salvatore <nicholassalvatore@Nicholass-MacBook-Air.local>
2023-12-12 08:21:44 -0800

    Add winston logger

--- scraper/src/scrapers/mystic.js
+++ scraper/src/scrapers/mystic.js
@@ -4,32 +4,18 @@ import * as utils from '../utils/utils.js'
 import { notify } from '../utils/notify.js'
 import { gpt } from '../utils/gpt.js'
 import { downloadMoviePoster } from '../utils/posters.js'
-
-
-export const mystic = { getShowings }
-
+import logger from '../utils/logger.js'
 
 async function getShowings() {
     try {
-        
-        // Initialize showings array
-        const showings = []
-
-        // Load page content
         const url = 'https://www.kvmr.org/venue/mystic-theater/'
         const html = await got(url).text()
         const $ = cheerio.load(html)
-
-        // Get element container for all films
+        const showings = []
         const films = $('.tribe-events-calendar-list__event-row')
-        
-        // Loop through each film container
+
         for (let film of films) {
-
-            // Apply cheerio wrapper to film element container
             const $film = $(film)
-
-            // Get film data
             const title = await getTitle($film)
             const date = getDate($film)
             const time = getTime($film)
@@ -38,7 +24,6 @@ async function getShowings() {
 
             downloadMoviePoster(posterUrl, title + '.jpg')
 
-            // Add film data to showings array
             showings.push({
                 title: title,
                 venue: 'Mystic Theater',
@@ -49,14 +34,10 @@ async function getShowings() {
             })
         }
 
-        console.log(`Retrieved ${showings.length} showings from Mystic Theater.`)
-
-        // Return showings array
+        logger.info(`Retrieved ${showings.length} showings from Mystic Theater.`)
         return showings
-
     } catch (error) {
-        console.error(error)
-
+        logger.error(error)
         notify.sendEmail(
             'Web Scraper Error: Mystic Theater', `
             <p>An error occurred:<p>
@@ -68,19 +49,16 @@ async function getShowings() {
     }
 }
 
-
 const getTitle = async film => {
     const titleStr = film.find('.tribe-events-calendar-list__event-title').first().text()
     const title = await gpt.getMovieTitle(titleStr)
     return utils.capitalize(title)
 }
 
-
 const getDate = film => {
     const dateStr = film.find('time.tribe-events-calendar-list__event-datetime').first().attr('datetime')
     return utils.formatDate(dateStr)
 }
-
 
 const getTime = film => {
     const datetime = film.find('.tribe-event-date-start').first().text()
@@ -89,11 +67,11 @@ const getTime = film => {
     return time
 }
 
-
 const getURL = film => film.find('.tribe-events-calendar-list__event-title-link').first().attr('href')
-
 
 const getPosterUrl = el => {
     const url = el.find('.tribe-events-calendar-list__event-featured-image').first().attr('src')
     return url
 }
+
+export const mystic = { getShowings }
