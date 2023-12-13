143dff9289d92f76054f1fdf990a6e7f418f0ce3
Nick Salvatore <nicholassalvatore@Nicholass-MacBook-Air.local>
2023-12-13 07:46:36 -0800

    Add notify call to catch block in index.js

--- scraper/src/index.js
+++ scraper/src/index.js
@@ -1,10 +1,17 @@
 import startScrapingService from './services/scrapingService.js'
 import logger from './utils/logger.js'
+import { notify } from './utils/notify.js'
 
 async function init() {
     try {
         await startScrapingService()
     } catch (error) {
+        await notify.sendEmail(
+            'Web Scraper Error', `
+            <p>An error occurred:<p>
+            <pre>${error.message}</pre>
+            <p>Please view logs for details.</p>`)
+
         logger.info('Terminating script')
         process.exit(1)
     }
