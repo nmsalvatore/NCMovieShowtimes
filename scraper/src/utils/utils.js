094715d9adfa7bc7ada3ddcac66e975b7d56ae0f
Nick Salvatore <nicholassalvatore@Nicholass-MacBook-Air.local>
2023-12-13 19:25:37 -0800

    Added shuffle function to utils.js

--- scraper/src/utils/utils.js
+++ scraper/src/utils/utils.js
@@ -4,6 +4,14 @@ export function delay(ms) {
     return new Promise(r => setTimeout(r, ms))
 }
 
+export function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array
+}
+
 export function removeRating(title) {
     return title.replace(/ \(?(G|PG|PG-13|R|NC-17|NR|\d+)?\)?$/, '')
 }
