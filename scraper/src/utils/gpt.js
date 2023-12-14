f00a545ecc020226e6dae1cc7ee5d4ebdfcbe960
Nick Salvatore <nicholassalvatore@Nicholass-MacBook-Air.local>
2023-12-13 19:41:18 -0800

    Added try-catch block with error logging and error re-throw

--- scraper/src/utils/gpt.js
+++ scraper/src/utils/gpt.js
@@ -1,21 +1,28 @@
 import 'dotenv/config'
 import OpenAI from 'openai'
+import logger from './logger.js'
 
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 })
 
 export async function getMovieTitle(str)  {
-    const completion = await openai.chat.completions.create({
-        messages: [{ 
-            role: 'system',
-            content: `Extract a movie title from the following string: "${str}". Return the title with proper capitalization and remove unecessary punctuation such as quotations around the full title name. If you're familiar with a movie that is being unnecessarily abbreviated, return the full title.`,
-        }],
-        model: 'gpt-4'
-    })
+    try {
+        const completion = await openai.chat.completions.create({
+            messages: [{ 
+                role: 'system',
+                content: `Extract a movie title from the following string: "${str}". Return the title with proper capitalization and remove unecessary punctuation such as quotations around the full title name. If you're familiar with a movie that is being unnecessarily abbreviated, return the full title.`,
+            }],
+            model: 'gpt-4'
+        })
+    
+        const response = completion.choices[0].message.content
+        return response
+    } catch (error) {
+        logger.error('Failed to extract movie title:', error)
+        throw error
+    }
 
-    const response = completion.choices[0].message.content
-    return response
 }
 
 export const gpt = { getMovieTitle }
