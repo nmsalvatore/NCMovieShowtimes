b057ef5cddab108e0897889fcc0a6a24e91374df
Nick Salvatore <nicholassalvatore@Nicholass-MacBook-Air.local>
2023-12-13 19:43:06 -0800

    Added log statements in scrapingService.js catch blocks

--- scraper/src/services/scrapingService.js
+++ scraper/src/services/scrapingService.js
@@ -2,12 +2,14 @@ import { onyx } from '../scrapers/onyx.js'
 import { sierra } from '../scrapers/sierra.js'
 import { mystic } from '../scrapers/mystic.js'
 import startDatabaseUpdateService from './databaseService.js'
+import logger from '../utils/logger.js'
 
 export default async function startScrapingService() {
     try {
         const showings = await getAllShowings()
         await startDatabaseUpdateService(showings)
     } catch (error) {
+        logger.error('Error starting scraping service:', error)
         throw error
     }
 }
@@ -26,6 +28,7 @@ async function getAllShowings() {
     
         return showings
     } catch (error) {
+        logger.error('Error retrieving all showings:', error)
         throw error
     }
 }
