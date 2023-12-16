165f368f4761eacc3d05d5c45646fb9fc59b13ac
Nick Salvatore <nicholassalvatore@Nicholass-MacBook-Air.local>
2023-12-15 16:45:52 -0800

    Added delay and transition on initial page load

--- frontend/src/routes/calendar/+page.svelte
+++ frontend/src/routes/calendar/+page.svelte
@@ -3,25 +3,32 @@
     import Showings from './Showings.svelte'
     import { activeRouteID, activeDate } from '$lib/stores.js'
     import { updateShowingsData } from '$lib/helpers/showings';
+    import { onMount } from 'svelte';
 
     export let data
     
     let showings = data.showings
     let dates = data.dates
     let activeDateValue = data.activeDateValue
+    let shouldRenderShowings = false
 
     activeRouteID.set(1)
     activeDate.set(activeDateValue)
 
-    let shouldRenderShowings = true
+    onMount(() => {
+        setRenderDelay(300)
+    })
 
     async function updateCalendar(e) {
         shouldRenderShowings = false
         showings = await updateShowingsData(e.detail)
+        setRenderDelay(300)
+    }
 
+    function setRenderDelay(ms) {
         setTimeout(() => {
             shouldRenderShowings = true
-        }, 300)
+        }, ms)
     }
 </script>
 
