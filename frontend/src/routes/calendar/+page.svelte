de3f6bf4f0623ed02b89e29e16aef0ceaacf85e6
Nick Salvatore <nicholassalvatore@Nicholass-MacBook-Air.local>
2023-12-17 08:05:58 -0800

    Removed render delay and fade

--- frontend/src/routes/calendar/+page.svelte
+++ frontend/src/routes/calendar/+page.svelte
@@ -3,50 +3,20 @@
     import Showings from './Showings.svelte'
     import { activeRouteID, activeDate } from '$lib/stores.js'
     import { updateShowingsData } from '$lib/helpers/showings';
-    import { onMount } from 'svelte';
 
     export let data
     
     let showings = data.showings
     let dates = data.dates
     let activeDateValue = data.activeDateValue
-    let shouldRenderShowings = false
 
     activeRouteID.set(1)
     activeDate.set(activeDateValue)
 
-    onMount(() => {
-        setRenderDelay(300)
-    })
-
     async function updateCalendar(e) {
-        shouldRenderShowings = false
         showings = await updateShowingsData(e.detail)
-        setRenderDelay(300)
-    }
-
-    function setRenderDelay(ms) {
-        setTimeout(() => {
-            shouldRenderShowings = true
-        }, ms)
     }
 </script>
 
 <Dates {dates} on:updateActiveDate={updateCalendar}/>
-
-<div class={shouldRenderShowings ? 'showings-container' : 'hidden'}>
-    <Showings {showings} />
-</div>
-
-<style>
-    .showings-container {
-        opacity: 1;
-        visibility: visible;
-        transition: opacity 1500ms, visible ease-in;
-    }
-
-    .hidden {
-        opacity: 0;
-        visibility: hidden;
-    }
-</style>
+<Showings {showings} />
