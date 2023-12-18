2359de75bc73ca2db059afbb67614233bb6e08ee
Nick Salvatore <nicholassalvatore@Nicholass-MacBook-Air.local>
2023-12-18 06:32:19 -0800

    Increased rate limit too 5000 requests

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ const corsOptions = {
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    limit: 500,
+    limit: 5000,
     standardHeaders: 'draft-7',
     legacyHeaders: false
 })
