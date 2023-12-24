059def262057db6a9098000ec2d042d436fb2c95
Nick Salvatore <nicholassalvatore@Nicholass-MacBook-Air.local>
2023-12-24 07:41:19 -0800

    Removed comment from posters.js

--- frontend/src/lib/helpers/posters.js
+++ frontend/src/lib/helpers/posters.js
@@ -1,7 +1,5 @@
 export function getPosterUrl(title) {
-    const apiUrl = import.meta.env.VITE_API_URL;
-    // Append a unique query string to the URL
-    const timestamp = new Date().getTime();
-    return `${apiUrl}/api/posters/${title}.jpg?timestamp=${timestamp}`;
+    const apiUrl = import.meta.env.VITE_API_URL
+    const timestamp = new Date().getTime()
+    return `${apiUrl}/api/posters/${title}.jpg?timestamp=${timestamp}`
 }
-
