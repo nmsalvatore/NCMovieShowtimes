d5711f304976811ec6c45a1a8d2b588423a8ef7f
Nick Salvatore <nmsalvatore@gmail.com>
2024-02-09 09:03:16 -0800

    Set store for activeCalendarPath to current date on layout mount

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,7 +1,14 @@
 <script>
-    import Header from "./Header.svelte";
-</script>
+    import Header from './Header.svelte'
+    import { activeCalendarPath } from '$lib/stores'
+    import { onMount } from 'svelte'
 
+    export let data
+
+    onMount(() => {
+        activeCalendarPath.set(data.activeCalendarPath)
+    })
+</script>
 
 <Header />
 <main>
