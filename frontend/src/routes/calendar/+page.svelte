452194fe9ada10b22cf66152f6c4351a0143766e
Nick Salvatore <nmsalvatore@gmail.com>
2024-02-09 09:06:15 -0800

    Clean up redirection on calendar route navigation

--- frontend/src/routes/calendar/+page.svelte
+++ frontend/src/routes/calendar/+page.svelte
@@ -1,28 +1,11 @@
 <script>
-    import Dates from './Dates.svelte'
-    import Showings from './Showings.svelte'
-    import { activeRouteID, activeDate } from '$lib/stores.js'
-    import { updateShowingsData } from '$lib/helpers/showings';
+    import { browser } from '$app/environment'
+    import { goto } from '$app/navigation'
 
     export let data
     
-    let showings = data.showings
-    let dates = data.dates
-    let activeDateValue = data.activeDateValue
-    let renderShowings = true
-
-    activeRouteID.set(1)
-    activeDate.set(activeDateValue)
-
-    async function updateCalendar(e) {
-        renderShowings = false
-        showings = await updateShowingsData(e.detail)
-        renderShowings = true
+    $: activeCalendarPath = data.activeCalendarPath
+    $: if (browser && data) {
+        goto(activeCalendarPath)
     }
 </script>
-
-<Dates {dates} on:updateActiveDate={updateCalendar}/>
-
-{#if renderShowings}
-    <Showings {showings} />
-{/if}
