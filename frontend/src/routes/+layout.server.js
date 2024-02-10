e1a87187926473d16be8a6598477b8d653cdc580
Nick Salvatore <nmsalvatore@gmail.com>
2024-02-10 12:16:42 -0800

    Redirect unknown paths to calendar

--- frontend/src/routes/+layout.server.js
+++ frontend/src/routes/+layout.server.js
@@ -1,6 +1,12 @@
 import { getTodayPath } from '$lib/helpers/dates.js'
+import { redirect } from '@sveltejs/kit'
 
-export function load() {
+export function load({ url }) {
+    const validPaths = ['/calendar', '/movies']
+    if (!validPaths.includes(url.pathname)) {
+        throw redirect(302, '/calendar')
+    }
+    
     const activeCalendarPath = getTodayPath()
     return { activeCalendarPath }
 }
