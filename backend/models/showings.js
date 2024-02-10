c13ad8bf1b648f6705f2fbe3450aeb2d5ae0169e
Nick Salvatore <nmsalvatore@gmail.com>
2024-02-10 11:50:55 -0800

    Removed date-specific showings API route for all showings route

--- backend/models/showings.js
+++ backend/models/showings.js
@@ -1,12 +1,11 @@
 import pool from '../config/db.js'
 
-export const getShowingsByDate = async date => {
+export const getAllShowings = async () => {
     const result = await pool.query(
         `SELECT showings.time, showings.date, movies.movie_title, movies.rating, movies.runtime, venues.venue_name, venues.address, showings.url
          FROM showings
          JOIN movies ON showings.movie_id = movies.movie_id
-         JOIN venues ON showings.venue_id = venues.venue_id
-         WHERE showings.date = $1`, [date]
+         JOIN venues ON showings.venue_id = venues.venue_id`
     )
 
     return result.rows
--- backend/routes/showings.js
+++ backend/routes/showings.js
@@ -1,17 +1,11 @@
 import { Router } from 'express'
-import { getShowingsByDate } from '../models/showings.js'
+import { getAllShowings } from '../models/showings.js'
 
 const router = Router()
 
 router.get('/', async (req, res) => {
-    const date = req.query.date;
-
-    if (!date) {
-        return res.status(400).json({ error: 'Date parameter is required.'})
-    }
-
     try {
-        const showings = await getShowingsByDate(date)
+        const showings = await getAllShowings()
         res.json(showings)
     } catch (err) {
         console.error(err)
