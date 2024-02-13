94bbf1b28b72cc9e745892874ff3738d71fd21f0
Nick Salvatore <nmsalvatore@gmail.com>
2024-02-13 10:53:49 -0800

    Removed store for current path

--- frontend/src/lib/stores.js
+++ frontend/src/lib/stores.js
@@ -1,6 +1,5 @@
 import { writable } from 'svelte/store'
 
 export const activeRouteID = writable(null)
-export const activeCalendarPath = writable(null)
 export const activeDate = writable(null)
 export const renderShowings = writable(false)
