fc10ecdbf82b02052a4ea6b58ddc007ff2310ee6
Nick Salvatore <nmsalvatore@gmail.com>
2024-02-15 07:33:29 -0800

    Fixed url for date fetching

--- frontend/src/lib/db.js
+++ frontend/src/lib/db.js
@@ -1,8 +1,10 @@
 export async function fetchDates() {
-    const res = await fetch('http://localhost:3000/api/dates')
+    const apiUrl = import.meta.env.VITE_API_URL
+    const res = await fetch(`${apiUrl}/api/dates`)
     const dates = await res.json()
     const today = new Date()
     today.setHours(0, 0, 0, 0)
+
     return dates.filter(dateString => {
         const dateParts = dateString.split('/')
         const date = new Date(dateParts[2], dateParts[0] - 1, dateParts[1])
@@ -14,11 +16,13 @@ export async function fetchAllShowings() {
     const apiUrl = import.meta.env.VITE_API_URL
     const response = await fetch(`${apiUrl}/api/showings`)
     const data = await response.json()
+
     return data
 }
 
 export async function fetchAllMovies() {
     const apiUrl = import.meta.env.VITE_API_URL
     const response = await fetch(`${apiUrl}/api/movies`)
+
     return await response.json()
 }
