04cd989e1ebfa7e2dfdd32e6788fc2cc18552840
Nick Salvatore <nmsalvatore@gmail.com>
2024-02-16 10:15:30 -0800

    Created now store

--- frontend/src/lib/stores.js
+++ frontend/src/lib/stores.js
@@ -3,3 +3,4 @@ import { writable } from 'svelte/store'
 export const activeRouteID = writable(null)
 export const activeDate = writable(null)
 export const renderShowings = writable(false)
+export const now = writable(null)
