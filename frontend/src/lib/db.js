15f7b584974c3293941dcfbcaa5c2ca2e875bf5e
Nick Salvatore <nmsalvatore@gmail.com>
2024-02-15 20:13:19 -0800

    Implemented authorization on api routes

--- frontend/src/lib/db.js
+++ frontend/src/lib/db.js
@@ -1,28 +1,54 @@
 export async function fetchDates() {
-    const apiUrl = import.meta.env.VITE_API_URL
-    const res = await fetch(`${apiUrl}/api/dates`)
-    const dates = await res.json()
-    const today = new Date()
-    today.setHours(0, 0, 0, 0)
-
-    return dates.filter(dateString => {
-        const dateParts = dateString.split('/')
-        const date = new Date(dateParts[2], dateParts[0] - 1, dateParts[1])
-        return date >= today
-    })
+    try {
+        const apiUrl = import.meta.env.VITE_API_URL
+        const res = await fetch(`${apiUrl}/api/dates`, {
+            method: 'GET',
+            headers: {
+                'X-API_KEY': import.meta.env.VITE_API_KEY,
+            }
+        })
+        const dates = await res.json()
+        const today = new Date()
+        today.setHours(0, 0, 0, 0)
+    
+        return dates.filter(dateString => {
+            const dateParts = dateString.split('/')
+            const date = new Date(dateParts[2], dateParts[0] - 1, dateParts[1])
+            return date >= today
+        })
+    } catch (error) {
+        throw new Error(error)
+    }
 }
 
 export async function fetchAllShowings() {
-    const apiUrl = import.meta.env.VITE_API_URL
-    const response = await fetch(`${apiUrl}/api/showings`)
-    const data = await response.json()
-
-    return data
+    try {
+        const apiUrl = import.meta.env.VITE_API_URL
+        const response = await fetch(`${apiUrl}/api/showings`, {
+            method: 'GET',
+            headers: {
+                'X-API_KEY': import.meta.env.VITE_API_KEY,
+            }
+        })
+        const showings = await response.json()
+        return showings
+    } catch (error) {
+        throw new Error(error)
+    }
 }
 
 export async function fetchAllMovies() {
-    const apiUrl = import.meta.env.VITE_API_URL
-    const response = await fetch(`${apiUrl}/api/movies`)
-
-    return await response.json()
+    try {
+        const apiUrl = import.meta.env.VITE_API_URL
+        const response = await fetch(`${apiUrl}/api/movies`, {
+                method: 'GET',
+                headers: {
+                    'X-API_KEY': import.meta.env.VITE_API_KEY,
+                }
+            })
+        const movies = await response.json()
+        return movies
+    } catch (error) {
+        throw new Error(error)
+    }
 }
--- frontend/src/lib/helpers/posters.js
+++ frontend/src/lib/helpers/posters.js
@@ -1,4 +1,4 @@
 export function getPosterUrl(title) {
     const apiUrl = import.meta.env.VITE_API_URL
-    return `${apiUrl}/api/posters/${title}.jpg`
+    return `${apiUrl}/posters/${title}.jpg`
 }
