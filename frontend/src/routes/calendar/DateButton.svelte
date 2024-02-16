90eaf18c5c564e685900834de3653cc53e2a7637
Nick Salvatore <nmsalvatore@gmail.com>
2024-02-16 10:13:34 -0800

    Created DateButton component

---
+++ frontend/src/routes/calendar/DateButton.svelte
@@ -0,0 +1,54 @@
+<script>
+    import { convertToShortDateString } from '$lib/helpers/dates.js'
+    import { activeDate, renderShowings } from '$lib/stores'
+
+    export let date
+
+    function handleButtonClick(date) {
+        if (date !== $activeDate) {
+            activeDate.set(date)
+            renderShowings.set(false)
+        }
+    }
+</script>
+
+<button 
+    class:active={ $activeDate === date }
+    on:click={ () => handleButtonClick(date) }>
+    { convertToShortDateString(date) }
+</button>
+
+<style>
+    button {
+        cursor: pointer;
+        padding: 12px;
+        width: 140px;
+        border: none;
+        border-radius: 5px;
+        margin-right: 12px;
+        color: #b4b4b4;
+        font-size: 16px;
+        font-weight: 400;
+        font-family: inherit;
+        background: #f4f4f4;
+        letter-spacing: 0.3px;
+    }
+
+    button:last-child {
+        margin-right: 0;
+    }
+
+    button.active {
+        background: lightcoral;
+        color: rgba(255, 255, 255, 0.98);
+        font-weight: 500;
+    }
+
+    @media only screen and (max-width: 600px) {
+        button {
+            width: 120px;
+            font-size: 14px;
+            padding: 10px;
+        }
+    }
+</style>
