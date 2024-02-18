a0ba8c77c198eb8ba363ef05af901ba4138ae984
Nick Salvatore <nmsalvatore@gmail.com>
2024-02-18 08:14:00 -0800

    Replaced nav buttons with icon buttons

--- frontend/src/routes/Navbar.svelte
+++ frontend/src/routes/Navbar.svelte
@@ -1,5 +1,7 @@
 <script>
     import { activeRouteID } from '$lib/stores.js'
+    import CalendarIcon from '$lib/assets/calendar.svg'
+    import FilmIcon from '$lib/assets/film.svg'
 </script>
 
 <nav>
@@ -8,14 +10,16 @@
         class:active={ $activeRouteID === 1 }
         on:click={ () => activeRouteID.set(1) }
     >
-        Calendar
+        <img src={ CalendarIcon } alt="Calendar Icon" />
+        <span>Calendar</span>
     </a>
     <a 
         href='/movies'
         class:active={ $activeRouteID === 2 }
         on:click={ () => activeRouteID.set(2) }
     >
-        All Movies
+        <img src={ FilmIcon } alt="Film Icon" />
+        <span>All Movies</span>
     </a>
 </nav>
 
@@ -27,39 +31,58 @@
     }
 
     a {
-        display: inline-block;
+        display: inline-flex;
+        flex-direction: column;
         text-align: center;
         text-decoration: none;
         cursor: pointer;
         background: #fff;
         border-radius: 4px;
-        width: 120px;
+        width: fit-content;
         padding: 10px;
         letter-spacing: 0.01rem;
-        font-size: 16px;
         font-weight: 500;
         color: #555;
-        background: #f4f4f4;
     }
 
     a:last-child {
-        margin-left: 1rem;
+        margin-left: 2rem;
     }
 
-    a.active {
-        color: #fff;
-        background: #444;
+    a > img {
+        height: 28px;
+        filter: invert(65%) sepia(0%) saturate(0%) hue-rotate(189deg) brightness(88%) contrast(81%);
+    }
+
+    a > span {
+        font-size: 12px;
+        margin-top: 4px;
+        color: #8f8f8f;
+    }
+
+    a.active > img {
+        filter: invert(31%) sepia(0%) saturate(1%) hue-rotate(90deg) brightness(95%) contrast(80%);
+    }
+
+    a.active > span {
+        color: #555;
     }
 
     @media only screen and (max-width: 600px) {
         a {
-            font-size: 14px;
             padding: 10px;
-            width: 100px;
         }
 
         a:last-child {
             margin-left: 12px;
+        }
+
+        a > img {
+            height: 24px;
+        }
+
+        a > span {
+            font-size: 10px;
         }
     }
 </style>
