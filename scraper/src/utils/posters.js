70ab5cc2192dce5918be0edf657c5785b6aacd69
Nick Salvatore <nmsalvatore@gmail.com>
2024-03-21 21:38:31 -0700

    Use error image as poster if error occurs

--- scraper/src/utils/posters.js
+++ scraper/src/utils/posters.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
-import { createWriteStream, existsSync } from 'fs';
+import { createWriteStream, existsSync, readFileSync } from 'fs';
 import { unlink, readdir } from 'node:fs/promises'
 import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
 import logger from '../utils/logger.js';
 import sharp from 'sharp'
+
 
 export async function downloadMoviePoster(url, filename) {
     const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -13,8 +14,6 @@ export async function downloadMoviePoster(url, filename) {
     if (existsSync(filepath)) {
         return;
     }
-
-    const writer = createWriteStream(filepath);
 
     try {
         const response = await axios({
@@ -25,20 +24,36 @@ export async function downloadMoviePoster(url, filename) {
 
         const resizedBuffer = await sharp(response.data)
             .resize(200)
-            .toBuffer()
+            .toBuffer();
 
-        const writer = createWriteStream(filepath)
-        writer.write(resizedBuffer, () => {
-            writer.close()
-        })
+        const writer = createWriteStream(filepath);
+            writer.write(resizedBuffer, () => {
+            writer.close();
+        });
 
         return new Promise((resolve, reject) => {
-            writer.on('finish', resolve)
-            writer.on('error', reject)
-        })
+            writer.on('finish', resolve);
+            writer.on('error', reject);
+        });
     } catch (error) {
-        logger.error(`Error downloading movie poster "${filename}"`)
-        throw error;
+        logger.error(`Error downloading movie poster "${filename}"`);
+
+        const placeholderFilepath = join(__dirname, '..', 'assets', 'image_unavailable.png');
+        const placeholderBuffer = readFileSync(placeholderFilepath);
+        const placeholderOutputPath = join(__dirname, '..', '..', '..', 'posters', filename);
+        const placeholderWriter = createWriteStream(placeholderOutputPath);
+        const resizedPlaceholderBuffer = await sharp(placeholderBuffer)
+            .resize(200)
+            .toBuffer();
+
+        placeholderWriter.write(resizedPlaceholderBuffer, () => {
+            placeholderWriter.close();
+        });
+
+        return new Promise((resolve, reject) => {
+            placeholderWriter.on('finish', resolve);
+            placeholderWriter.on('error', reject);
+        });
     }
 }
 
